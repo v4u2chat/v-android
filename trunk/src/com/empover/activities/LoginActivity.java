9a0c46f0c4cf19d07a784cc242864421d523e090
v4u2chat@gmail.com <v4u2chat@gmail.com@3dc1f082-1473-bd96-b2ac-5e6542d17e8c>
2013-01-03 10:03:49 +0000

    Basic Application Setup

---
+++ trunk/src/com/empover/activities/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.empover.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity implements OnClickListener
+{
+	
+	private EditText loginId;
+	private EditText pasword;
+	private Button loginBtn;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.login);
+		
+		loginId		=	(EditText)findViewById(R.id.username);
+		pasword		=	(EditText)findViewById(R.id.password);
+		loginBtn	=	(Button)findViewById(R.id.lognBtn);
+		
+		loginBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) 
+	{
+		System.out.println(loginId.getText()+"---------"+pasword.getText());
+		Toast.makeText(this, loginId.getText()+"---------"+pasword.getText(), Toast.LENGTH_SHORT).show();
+		
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		menu.add(0,1,0,"Test");
+		return super.onCreateOptionsMenu(menu);
+	}
+}
